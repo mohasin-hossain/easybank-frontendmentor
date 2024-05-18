4a5a72d09a8c41c7131bdd9bb440fea4757031e3
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-05-18 22:44:00 +0600

    Mobile Nav Close function on NavLink click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-const hamburgerIcon = document.querySelector('.nav__hamburger');
-const navList = document.querySelector('.nav__list');
-const navLinks = document.querySelectorAll('.nav__link');
+const hamburgerIcon = document.querySelector(".nav__hamburger");
+const navList = document.querySelector(".nav__list");
+const navLinks = document.querySelectorAll(".nav__link");
 
-hamburgerIcon.addEventListener('click', () => {
-    hamburgerIcon.classList.toggle('active');
-    navList.classList.toggle('active');
-})
+hamburgerIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.toggle("active");
+  navList.classList.toggle("active");
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    hamburgerIcon.classList.remove("active");
+    navList.classList.remove("active");
+  });
+});
